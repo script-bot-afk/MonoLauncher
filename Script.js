28e53a2644409cd0dc04f71a92d21bb3a48c6144
NVT <nicowv123@gmail.com>
2026-06-25 21:15:20 -0600

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+const downloadBtn = document.getElementById("downloadBtn");
+
+downloadBtn.addEventListener("click", () => {
+    // Replace this with your actual APK link
+    window.location.href =
+        "https://github.com/yourusername/mono-launcher/releases/latest";
+});
